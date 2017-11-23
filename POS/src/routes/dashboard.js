061ff4d9354fde0053ba3b4e3fe1fb3a47aa28f7
Gita Prawira <gita@logisfleet.com>
2017-11-23 15:26:42 +0700

    [POS] Innital Commit

---
+++ POS/src/routes/dashboard.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function(req, res, next) {
+  res.render('dashboard');
+});
+
+router.post('/', function(req, res, next) {
+  res.render('dashboard');
+});
+
+module.exports = router;
